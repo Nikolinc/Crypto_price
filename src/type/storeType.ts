0d6cc9cbdf2567b593a4ecc26036f1f338b446c0
Nikolinc <tsigulskynikita@gmail.com>
2022-12-18 20:22:32 +0500

    add interface

---
+++ src/type/storeType.ts
@@ -0,0 +1,16 @@
+
+export default interface coinType{
+     item: {
+         name: string;
+         large: string;
+         id: number;
+         price_btc: number;
+     };
+}
+
+export default interface coinArrayType{
+    name: string;
+    large: string;
+    id: number;
+    price_btc: number;
+}
