f0df2cd908b47b85598642f7dec87d9ea9c51315
Nikolinc <tsigulskynikita@gmail.com>
2022-12-25 02:40:28 +0500

    add loader

---
+++ src/component/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading(){
+    return(
+        <div className=" right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 h-4/5 ">
+        <div
+            className="border-t-transparent border-solid animate-spin  rounded-full border-b-emerald-800 border-8 h-64 w-64"></div>
+    </div>)
+}
