1d377cefeaf1f96b48052880adf715cb7573aa05
Nikolinc <tsigulskynikita@gmail.com>
2022-12-25 17:14:39 +0500

    Updates

---
+++ precache-manifest.c1a9c84b5b1333c09f542b09b8c55eff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be635aa5054b373b615c",
+    "url": "/static/css/main.12a4dd99.chunk.css"
+  },
+  {
+    "revision": "be635aa5054b373b615c",
+    "url": "/static/js/main.4533a304.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "39a8aca6eb397dc37ed0",
+    "url": "/static/js/2.7e43141d.chunk.js"
+  },
+  {
+    "revision": "1cf8d03e56baf1218009049d8cb4cae3",
+    "url": "/index.html"
+  }
+];
