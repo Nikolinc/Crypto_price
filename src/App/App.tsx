c6162bc88f841ca603e4bb618dd57d88656e6946
Nikolinc <tsigulskynikita@gmail.com>
2023-01-10 02:25:49 +0500

    add index page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import Home from '../pages/Home'
-import Show from '../pages/Show'
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Show from "../pages/Show";
 import Header from "../component/header";
 import Footer from "../component/footers";
 
 export default function App() {
   return (
     <HashRouter>
-        <Header/>
+      <Header />
       <Routes>
-        <Route path = '/' element = {<Home/>}/>
-        <Route path = '/:id' element ={<Show/>}/>
+        <Route index element={<Home />} />
+        <Route path="/:id" element={<Show />} />
       </Routes>
-        <Footer/>
-  </HashRouter>
-  )
+      <Footer />
+    </HashRouter>
+  );
 }
